9e8f715ff11a417031f7827b3949ddd7bfb91dc7
Nicolas Degory <ndegory@axway.com>
2016-07-21 09:10:13 +0000

    CI - test also the existence of measurements in telegraf DB, do some retries

--- sut/test.sh
+++ sut/test.sh
@@ -2,27 +2,53 @@
 
 INFLUXDB_HOST=${INFLUXDB_HOST:-influxdb}
 
+echo -n "test 1... "
 # give time to influxdb to be up
 sleep 2
-
 curl -I $INFLUXDB_HOST:8083 2>/dev/null | grep -q "HTTP/1.1 200 OK"
 if [[ $? -ne 0 ]]; then
+  echo
   echo "Influxdb:8083 failed"
   curl -I $INFLUXDB_HOST:8083
   exit 1
 fi
+echo "[OK]"
+
+echo -n "test 2... "
 curl -I $INFLUXDB_HOST:8086/ping 2>/dev/null | grep -q "HTTP/1.1 204 No Content"
 if [[ $? -ne 0 ]]; then
+  echo
   echo "Influxdb:8086 ping failed"
   exit 1
 fi
+echo "[OK]"
 
+echo -n "test 3... "
+r="false"
+i=0
 # give time to telegraf to send data
-sleep 2
-r=$(curl -GET "http://$INFLUXDB_HOST:8086/query?pretty=true" --data-urlencode "db=telegraf" --data-urlencode "q=SELECT usage_total FROM docker_container_cpu limit 1" 2>/dev/null | jq -r '.results[0] | has("series")')
+while [[ "x$r" != "xtrue" ]]; do
+  sleep 1
+  r=$(curl -GET "http://$INFLUXDB_HOST:8086/query" --data-urlencode "db=telegraf" --data-urlencode "q=SHOW MEASUREMENTS" 2>/dev/null | jq -r '.results[0] | has("series")')
+  ((i++))
+  if [[ $i -gt 20 ]]; then break; fi
+done
 if [[ "x$r" != "xtrue" ]]; then
-  echo "Influxdb telegraf db has no docker_container_cpu measurements ($r)"
+  echo
+  echo "Influxdb telegraf db has no measurements ($r)"
+  curl -GET "http://$INFLUXDB_HOST:8086/query" --data-urlencode "db=telegraf" --data-urlencode "q=SHOW MEASUREMENTS"
   exit 1
 fi
+echo "[OK]"
 
-echo "all tests are OK"
+echo -n "test 4... "
+r=$(curl -GET "http://$INFLUXDB_HOST:8086/query" --data-urlencode "db=telegraf" --data-urlencode "q=SELECT usage_total FROM docker_container_cpu limit 1" 2>/dev/null | jq -r '.results[0] | has("series")')
+if [[ "x$r" != "xtrue" ]]; then
+  echo
+  echo "Influxdb telegraf db has no docker_container_cpu measurements ($r)"
+  curl -GET "http://$INFLUXDB_HOST:8086/query" --data-urlencode "db=telegraf" --data-urlencode "q=SELECT usage_total FROM docker_container_cpu limit 1"
+  exit 1
+fi
+echo "[OK]"
+
+echo "all tests passed successfully"
