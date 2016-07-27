fb70dc878850e2d8d33cae00bb3eafb1c84dbac7
Nicolas Degory <ndegory@axway.com>
2016-07-27 13:06:53 +0000

    amp-pilot is now expected to be mounted, else it's disabled

--- run.sh
+++ run.sh
@@ -204,10 +204,11 @@ echo "=> Starting InfluxDB in foreground ..."
 CMD="influxd"
 CMDARGS="-config=${CONFIG_FILE}"
 export AMPPILOT_LAUNCH_CMD="$CMD $CMDARGS"
-if [[ -n "$CONSUL" && -n "$PILOT" ]]; then
+if [[ -n "$CONSUL" && -x "$PILOT" ]]; then
     echo "registering in Consul with $PILOT"
-    exec "$PILOT" "$CMD" $CMDARGS
+    # set variable to make sure amp-pilot won't define the command by itself
+    AMPPILOT_STANDALONE=1
+    exec "$PILOT"
 else
-    echo "not registering in Consul"
     exec "$CMD" $CMDARGS
 fi
